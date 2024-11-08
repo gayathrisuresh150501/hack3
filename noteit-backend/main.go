e3bb61cc1acf5ceaadb3fd14f2d4a0d026efd024
Gayathri Suresh <gayathri.suresh@loginradius.com>
2024-11-08 18:41:57 +0530

    Fix Panic

--- noteit-backend/main.go
+++ noteit-backend/main.go
@@ -25,8 +25,8 @@ func main() {
 
 	server.POST("/api/notes", handlers.CreateNote)
 	server.POST("/api/notes/{uid}", handlers.CreateNoteForUser)
-	server.GET("/api/notes/{id}", handlers.GetNote)
-	server.GET("/api/notes/all", handlers.GetAllNotes)
+	// server.GET("/api/notes/{id}", handlers.GetNote)
+	server.GET("/api/notes/{id}", handlers.GetAllNotes)
 	server.PUT("/api/notes/{id}", handlers.UpdateNote)
 	server.DELETE("/api/notes/{id}", handlers.DeleteNote)
 	server.POST("/plan", handlers.AddPlan)
